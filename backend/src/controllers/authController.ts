dee8869e5c98bdfa649a5ba16b80c9a4b2000c97
Mohammad Moghnieh <mohammadmoghnieh5@gmail.com>
2026-08-19 22:51:44 +0300

    changes

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,3 +1,4 @@
+import crypto from "crypto";
 import { Request, Response, NextFunction } from "express";
 import { supabase } from "../utils/supabaseClient";
 import { ApiError } from "../middleware/errorHandler";
